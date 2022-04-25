e38402db732ee96f8e24723aca23a5ea274ec5a7
Jorge Simoes - Desktop <azgraalsoftware@gmail.com>
2022-04-25 04:38:55 +0100

    hotfix

--- src/main/java/io/github/azgraal/utilitarios/Verificacao.java
+++ src/main/java/io/github/azgraal/utilitarios/Verificacao.java
@@ -2,8 +2,16 @@ package io.github.azgraal.utilitarios;
 
 import static io.github.azgraal.utilitarios.Processamento.converterVirgulaEmPontoDecimal;
 
+/**
+ * Classe com métodos estáticos destinados a fazer verificações de várias condições.
+ */
 public class Verificacao {
 
+    /**
+     * Verifica se a string passada como parâmetro é um número inteiro válido.
+     * @param string a string a ser verificada.
+     * @return diz se é a condição é verdadeira ou falsa.
+     */
     public static boolean isStringInteiro(String string){
         try {
             int num = Integer.parseInt(string);
@@ -13,6 +21,11 @@ public class Verificacao {
         }
     }
 
+    /**
+     * Verifica se a string passada como parâmetro é um float válido.
+     * @param string a string a ser verificada.
+     * @return diz se é a condição é verdadeira ou falsa.
+     */
     public static boolean isStringFloat(String string){
         try {
             converterVirgulaEmPontoDecimal(string);
@@ -23,6 +36,11 @@ public class Verificacao {
         }
     }
 
+    /**
+     * Verifica se a string passada como parâmetro é um double válido.
+     * @param string a string a ser verificada.
+     * @return diz se é a condição é verdadeira ou falsa.
+     */
     public static boolean isStringDouble(String string){
         try {
             converterVirgulaEmPontoDecimal(string);
@@ -33,6 +51,11 @@ public class Verificacao {
         }
     }
 
+    /**
+     * Verifica se a string passada como parâmetro não é nula, não está vazia nem está em branco.
+     * @param string a string a ser verificada.
+     * @return diz se é a condição é verdadeira ou falsa.
+     */
     public static boolean isStringValida(String string){
         if (string != null && !string.isEmpty() && !string.isBlank()){
             return true;
